6a48a765b8067087a6549558f5022f2c229b8036
Pablo <pablogcg05@gmail.com>
2023-04-14 09:11:36 +0200

    Introducción condiciones para par o impar

--- Program.cs
+++ Program.cs
@@ -3,16 +3,24 @@ var primerNumero = Console.ReadLine();
 int numero1 = int.Parse(primerNumero);
 Console.WriteLine("Introduzca el segudno número"); 
 var segundoNumero = Console.ReadLine();
-int numero2 = int.Parse(primerNumero);
+int numero2 = int.Parse(segundoNumero);
 if (numero1 > numero2)
 {
-    Console.WriteLine("El primer número introducido es mayor al segundo");
+    Console.WriteLine("El " + numero1 + " es mayor al " + numero2);
 }
 else if (numero1 < numero2)
 {
-    Console.WriteLine("El primer número introducido es menor al segundo");
+    Console.WriteLine("El " + numero1 + " es menor al " + numero2);
 }
 else
 {
-    Console.WriteLine("El primer número introducido es igual al segundo");
+    Console.WriteLine("El " + numero1 + " es igual " + numero2);
+}
+if (numero2 % 2 == 1)
+{
+    Console.WriteLine("El " + numero2 + " es impar");
+}
+else
+{
+    Console.WriteLine("El " + numero2 + " es par");
 }
