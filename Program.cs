3fcefc1910dfbf5f40854f38e89628296708d3b4
Pablo <pablogcg05@gmail.com>
2023-04-14 09:00:06 +0200

    Condiciones de mayor o menor

--- Program.cs
+++ Program.cs
@@ -4,3 +4,15 @@ int numero1 = int.Parse(primerNumero);
 Console.WriteLine("Introduzca el segudno número"); 
 var segundoNumero = Console.ReadLine();
 int numero2 = int.Parse(primerNumero);
+if (numero1 > numero2)
+{
+    Console.WriteLine("El primer número introducido es mayor al segundo");
+}
+else if (numero1 < numero2)
+{
+    Console.WriteLine("El primer número introducido es menor al segundo");
+}
+else
+{
+    Console.WriteLine("El primer número introducido es igual al segundo");
+}
