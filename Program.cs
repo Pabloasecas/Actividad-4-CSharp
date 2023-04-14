dda833f83702ab308eecb89b7e8d4ea56a1d8341
Pablo <pablogcg05@gmail.com>
2023-04-14 08:49:52 +0200

    Creación archivos y código que pide dos números

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Introduzca el primer número"); 
+var primerNumero = Console.ReadLine();
+int numero1 = int.Parse(primerNumero);
+Console.WriteLine("Introduzca el segudno número"); 
+var segundoNumero = Console.ReadLine();
+int numero2 = int.Parse(primerNumero);
